15b0ecaad7d742e14ff32dd2d89434e3e0b7c750
narcpat <narcissearts@gmail.com>
2022-07-13 20:46:54 -0400

    completed user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,4 +16,51 @@ const userController = {
         res.sendStatus(400);
       });
   },
+
+  // get user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // create a new user
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
+
+  // update a user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "User not found" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // delete a user
+  deleteUser({ params }, res) {
+    User.findByIdAndDelete({ _id: params.id })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
 };
+
+module.exports = userController;
